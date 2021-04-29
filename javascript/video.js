71eb8bed260fc9b453df9dea0bac0cad8c220957
高大晋 <dajin.gao@gmail.com>
2021-04-30 05:40:18 +0900

    ver 0.1

---
+++ javascript/video.js
@@ -0,0 +1,10 @@
+function displayIFrame() {
+    $(".video").css("visibility", "visible");
+    $(".videoIframe").css("width", "640");
+    $(".videoIframe").css("height", "360");
+  }
+  
+  function closeIFrame() {
+    $(".video").css("visibility", "hidden");
+  }
+  
